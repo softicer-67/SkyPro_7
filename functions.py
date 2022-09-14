598f49d1c218546bcf0f429b32bdd86e28581c2c
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-09-15 03:53:54 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,51 @@
+import json
+
+with open('students.json', 'r', encoding='utf-8') as f:
+    stud = json.load(f)
+
+with open('professions.json', 'r', encoding='utf-8') as f:
+    prof = json.load(f)
+
+
+def load_students():
+    for text in stud:
+        return text['full_name']
+
+
+def load_professions():
+    for text in stud:
+        return text['skills']
+
+
+def get_student_by_pk(pk):
+    for text in stud:
+        if str(pk) == str(text['pk']):
+            student = text['full_name']
+            return student
+
+
+def get_skill(pk):
+    for text in stud:
+        if str(pk) == str(text['pk']):
+            lang = text['skills']
+            return lang
+
+
+def get_profession_by_title(title):
+    for text in prof:
+        if title in text['title']:
+            skils = text['skills']
+            return skils
+
+
+def check_fitness(student, profession, skill):
+    yes = []
+    no = []
+    for i in profession:
+        if i in skill:
+            yes.append(i)
+        elif i in profession or i in skill:
+            no.append(i)
+    print(f'Пригодность {100 // (len(yes) + len(no)) * len(yes)}%')
+    print(f'{student} знает {yes}')
+    print(f'{student} не знает {no}')
